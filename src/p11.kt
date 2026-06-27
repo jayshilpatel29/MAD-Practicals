8f72043dbca6937283019961a8477d017fd06884
JAYSHIL <pateljayshil56@gmail.com>
2026-06-27 09:39:10 +0530

    Initial commit

---
+++ src/p11.kt
@@ -0,0 +1,38 @@
+fun main() {
+
+    var a: Int = 10
+    var b: Float = 1.5F
+    var c: Double = 10.5
+    var d: Char = 'J'
+    var e: String = "Jay"
+    var f: Boolean = true
+    var g: Long = 123456789L
+    var h: Short = 100
+    var i: Byte = 127
+
+    val x=100
+      g = 200
+    val j = "jayshil"
+    var k: Float = 1.5F
+    var l: Char = 'J'
+
+
+    println("a = " + a)
+    println("b = " + b)
+    println("c = " + c)
+    println("d = " + d)
+    println("e = " + e)
+    println("f = " + f)
+    println("g = " + g)
+    println("h = " + h)
+    println("i = " + i)
+    println("x = " + x)
+    println("j = $j")
+    println("Value of k is $k")
+    println("Value of l is $l")
+    println("k + 2 = ${k + 2}")
+    println("Unicode value of l = ${l.code}")
+    println("k is $k and l is $l")
+
+
+}
---
+++ src/p12.kt
@@ -0,0 +1,10 @@
+fun main () {
+    val i = 100
+    val j : Double = i.toDouble()
+    val s = "1000"
+    val a: Int = s.toInt()
+    val b: Double = a.toDouble()
+
+    println("i = $i, j = $j, s = $s, s = $b" )
+
+}
