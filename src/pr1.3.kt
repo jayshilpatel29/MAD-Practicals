2d897a63513c3eaba92dc9f774a4e7465796f194
JAYSHIL <pateljayshil56@gmail.com>
2026-06-30 13:23:37 +0530

    Initial commit

---
+++ src/pr1.3.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    println("Enter Enrollment No.: ")
+    val enrollmentNo: String = readln()
+
+    println("Enter Name: ")
+    val name: String = readln()
+
+    println("Enter Branch: ")
+    val branch: String = readln()
+
+    println("Enter Class: ")
+    val studentClass: String = readln()
+
+    println("Enter Batch: ")
+    val batch: String = readln()
+
+    println("college name:")
+    val collegename: String = readln()
+
+    println("university name:")
+    val universityname: String = readln()
+
+    println("Age:")
+    val age: String = readln()
+}
