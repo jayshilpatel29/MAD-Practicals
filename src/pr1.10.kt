cbf7bcf0c995caf6bfb2580f6bd55280d02a9403
jayshil patel <pateljayshil56@gmail.com>
2026-08-25 00:09:17 +0530

    Initial commit

--- src/pr1.10.kt
+++ src/pr1.10.kt
@@ -1,18 +1,69 @@
-open class car(var model: String){
+class Car(
+    var company: String,
+    var year: Int,
+    var owner: String,
+    var milesDrive: Int,
+    var originalPrice: Double
+) {
 
-    var price: Double=100000.0
+    fun currentPrice(): Double {
+        return originalPrice - (milesDrive * 10)
+    }
 
+    fun displayInformation() {
+        println("--------------------")
+        println("Car Information: $company, $year")
+        println("Car Owner: $owner")
+        println("Miles Driven: $milesDrive")
+        println("Original Car Price: $originalPrice")
+        println("Current Car Price: ${currentPrice()}")
+        println("--------------------")
+    }
 }
-class suzuki(m:String):car( m){
 
-constructor(m:String,p: Double):this(m)
-{
+fun main() {
 
-}
-}
-fun main()
-{
-    val car = suzuki("alto")
-    println(car.model)
-    println(car.price)
+    val car1 = Car(
+        "BMW",
+        2018,
+        "Aman",
+        105,
+        100000.0
+    )
+    car1.displayInformation()
+
+    val car2 = Car(
+        "BMW",
+        2019,
+        "Karan",
+        20,
+        400000.0
+    )
+    car2.displayInformation()
+
+    val carList = ArrayList<Car>()
+
+    carList.add(
+        Car(
+            "Toyota",
+            2017,
+            "KJS",
+            100,
+            1080000.0
+        )
+    )
+
+    carList.add(
+        Car(
+            "Maruti",
+            2020,
+            "NPP",
+            200,
+            4000000.0
+        )
+    )
+
+    for (car in carList) {
+        car.displayInformation()
+    }
 }
